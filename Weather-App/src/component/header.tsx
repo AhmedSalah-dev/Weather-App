1c981543e771deb5f9488b7cdce4b60fab680468
AhmedSalah-dev <Midoelshal78@gmail.com>
2025-01-19 21:24:18 +0200

    historySearch

--- Weather-App/src/component/header.tsx
+++ Weather-App/src/component/header.tsx
@@ -3,6 +3,7 @@ import darkLogo from '../assets/logo.png'
 import lightLogo from '../assets/logo2.png'
 import{ useTheme } from '../context/Theme-provider';
 import { Moon, Sun } from "lucide-react";
+import CitySearch from './CitySearch';
 
 
 const Header = () => {
@@ -21,21 +22,23 @@ const Header = () => {
           className='h-14' />
         </Link>
 
-        <div>
-             {/* search  */}
-             {/* theme toggle  */}
-        </div>
-        <div 
-         onClick={()=> setTheme(isDark? 'light': 'dark') }
-          className={`flex items-center cursor-pointer transition-transform duration-500
-          ${isDark ?"rotate-180":"rotate-0"} 
-          `} >
+        
+        <div className='flex gap-4'>
+          <CitySearch />
+         
+          <div
+            
+          onClick={()=> setTheme(isDark? 'light': 'dark') }
+            className={`flex items-center cursor-pointer transition-transform duration-500
+            ${isDark ?"rotate-180":"rotate-0"} 
+            `} >
 
-            {isDark? (
-                <Sun className='h-6 w-6 text-yellow-500 rotate-0 transition-all cursor-pointer' />
-            ):(
-            <Moon className='h-6 w-6 text-gray-500 rotate-0 transition cursor-pointer'/>
-           )}
+              {isDark? (
+                  <Sun className='h-6 w-6 text-yellow-500 rotate-0 transition-all cursor-pointer' />
+              ):(
+              <Moon className='h-6 w-6 text-gray-500 rotate-0 transition cursor-pointer'/>
+            )}
+          </div>
         </div>
     </div>
   </header>
